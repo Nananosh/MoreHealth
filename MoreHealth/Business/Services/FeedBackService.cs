d083852fa01620e282ce6e367b109b7ebe2f045b
Дмитрий Медведев <greatsmith184@gmail.com>
2021-11-29 18:18:53 +0300

    MH-2 Added serivce and interface.

---
+++ MoreHealth/Business/Services/FeedBackService.cs
@@ -0,0 +1,18 @@
+﻿using MoreHealth.Business.Interfaces;
+using MoreHealth.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MoreHealth.Business.Services
+{
+    public class FeedBackService : IFeedBackService
+    {
+
+        public IEnumerable<Doctor> GetAllDoctors(ApplicationContext db)
+        {
+            IEnumerable<Doctor> doctors = db.Doctor;
+
+            return doctors;
+        }
+    }
+}
